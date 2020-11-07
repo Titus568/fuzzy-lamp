30bae8942373baab1dab25dc39e80f3a3eacce81
Titus568 <74080090+Titus568@users.noreply.github.com>
2020-11-06 16:34:02 -0800

    Add files via upload
    I decided to make my own Celsius calculator that operates in Python. It will turn an imputed Celsius number into Fahrenheit.

---
+++ celsiusCalc.py
@@ -0,0 +1,6 @@
+
+print ("The following will convert Celsius to Fahrenheit")
+celsius= int(input("Enter the degrees in Celsius here "))
+fahrenheit = (9 / 5) * (celsius) + 32
+print ("Celsius Fahrenheit")
+print(celsius,"\t",fahrenheit)
